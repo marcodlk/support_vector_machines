f9c6d2748726799b6df346d9acd8a854955c745c
Marco de Lannoy Kobayashi <markobrazil93@hawaii.edu>
2017-10-11 09:04:33 -1000

    Major refactor. Make svmlab.py more flexible and functional; rename interface appropriately. Add the ability to produce heatmaps of the optimal parameter grid search.

--- iris_svc_study.py
+++ iris_svc_study.py
@@ -1,4 +1,5 @@
 from sklearn.datasets import load_iris
+from matplotlib import colors
 import numpy as np
 import svmlab
 
@@ -18,6 +19,7 @@ INTERVAL = 0.5
 
 # output directory name
 ANIM_DIR = 'animations/'
+HEAT_DIR = 'heatmaps/'
 
 # SVC filenames
 svc_linear_C_fname = ANIM_DIR + 'iris_svc_linear_C.mp4'
@@ -25,69 +27,141 @@ svc_poly_C_fname = ANIM_DIR + 'iris_svc_poly_C.mp4'
 svc_poly_gamma_fname = ANIM_DIR + 'iris_svc_poly_gamma.mp4'
 svc_rbf_C_fname = ANIM_DIR + 'iris_svc_rbf_C.mp4'
 svc_rbf_gamma_fname = ANIM_DIR + 'iris_svc_rbf_gamma.mp4'
-svc_rbf_dual_fname = ANIM_DIR + 'iris_svc_rbf_dual.mp4'
+svc_rbf_both_fname = ANIM_DIR + 'iris_svc_rbf_both.mp4'
+# heatmaps
+svc_linear_grid_search = HEAT_DIR + 'iris_svc_linear_grid_search.png'
+svc_poly_grid_search = HEAT_DIR + 'iris_svc_poly_grid_search.png'
+svc_rbf_grid_search = HEAT_DIR + 'iris_svc_rbf_grid_search.png'
 
 # NuSVC filenames
 nusvc_linear_nu_fname = ANIM_DIR + 'iris_nusvc_linear_nu.mp4'
+nusvc_poly_nu_fname = ANIM_DIR + 'iris_nusvc_poly_nu.mp4'
+nusvc_poly_gamma_fname = ANIM_DIR + 'iris_nusvc_poly_gamma.mp4'
+nusvc_rbf_nu_fname = ANIM_DIR + 'iris_nusvc_rbf_nu.mp4'
+nusvc_rbf_gamma_fname = ANIM_DIR + 'iris_svc_rbf_gamma.mp4'
+# heatmaps
+nusvc_linear_grid_search = HEAT_DIR + 'iris_nusvc_linear_grid_search.png'
+nusvc_poly_grid_search = HEAT_DIR + 'iris_nusvc_poly_grid_search.png'
+nusvc_rbf_grid_search = HEAT_DIR + 'iris_nusvc_rbf_grid_search.png'
 
 # generate C, nu, and gamma static lists
-static_C_list = [1.0 for _ in range(30)]
-static_nu_list = [0.5 for _ in range(30)]
-static_gamma_list = [1.0 for _ in range(30)]
+static_C_range = [1.0 for _ in range(30)]
+static_nu_range = [0.5 for _ in range(30)]
+static_gamma_range = [1.0 for _ in range(30)]
 
 # generate C, nu, and gamma dynamic lists
-dynamic_C_list = np.logspace(-2, 3, 30)
-dynamic_nu_list = np.linspace(0., 0.9, 50)[1:-1] # 0 and 1 are infeasible nu values, strip **
-dynamic_gamma_list = np.logspace(-2, 3, 30)
+dynamic_C_range = np.logspace(-2, 3, 30)
+dynamic_nu_range = np.linspace(0., 0.9, 50)[1:-1] # 0 and 1 are infeasible nu values, strip **
+dynamic_gamma_range = np.logspace(-2, 3, 30)
 
 # ** note it seems that actually nu values greater than 0.9 are infeasible
 
 # initialize svm lab with iris dataset
-print('-- initializing support vector machines lab ')
+print('-- initializing support vector machines lab')
 lab = svmlab.SVMLab(x_data, y_data)
 
 # svc, linear kernel, study effect of C parameter (softmargin)
 print('-- studying effect of C parameter on SVC, linear kernel ')
-lab.study_svc(kernel='linear', C_list=dynamic_C_list)
-lab.visualize_results(svc_linear_C_fname, interval=INTERVAL)
+lab.svm_animation(
+    svm_=dict(impl='svc', kernel='linear', dfs='ovr'),
+    range_=dict(C=dynamic_C_range, gamma=None),
+    animation_=dict(filename=svc_linear_C_fname)
+)
 print('   ... saved to file %s ' % svc_linear_C_fname)
 
 # svc, poly kernel, study effect of C parameter (softmargin)
 print('-- studying effect of C parameter on SVC, poly kernel ')
-lab.study_svc(kernel='poly', 
-              C_list=dynamic_C_list,
-              gamma_list=static_gamma_list)
-lab.visualize_results(svc_poly_C_fname, interval=INTERVAL)
+lab.svm_animation(
+    svm_=dict(impl='svc', kernel='poly', dfs='ovr'),
+    range_=dict(C=dynamic_C_range, gamma=static_gamma_range),
+    animation_=dict(filename=svc_poly_C_fname)
+)
 print('   ... saved to file %s ' % svc_poly_C_fname)
 
 # svc, rbf kernel, study effect of C parameter (softmargin)
 print('-- studying effect of C parameter on SVC, rbf kernel ')
-lab.study_svc(kernel='rbf', 
-              C_list=dynamic_C_list,
-              gamma_list=static_gamma_list)
-lab.visualize_results(svc_rbf_C_fname, interval=INTERVAL)
+lab.svm_animation(
+    svm_=dict(impl='svc', kernel='rbf', dfs='ovr'),
+    range_=dict(C=dynamic_C_range, gamma=static_gamma_range),
+    animation_=dict(filename=svc_rbf_C_fname)
+)
 print('   ... saved to file %s ' % svc_rbf_C_fname)
 
 # svc, rbf kernel, study effect of gamma parameter (kernel coefficient)
 print('-- studying effect of gamma parameter on SVC, rbf kernel ')
-lab.study_svc(kernel='rbf', 
-              C_list=static_C_list,
-              gamma_list=dynamic_gamma_list)
-lab.visualize_results(svc_rbf_gamma_fname, interval=INTERVAL)
+lab.svm_animation(
+    svm_=dict(impl='svc', kernel='rbf', dfs='ovr'),
+    range_=dict(C=static_C_range, gamma=dynamic_gamma_range),
+    animation_=dict(filename=svc_rbf_gamma_fname)
+)
 print('   ... saved to file %s ' % svc_rbf_gamma_fname)
 
 # svc, rbf kernel, study effect of both C and gamma parameters (jointly incrementing)
 print('-- studying effect of both C and gamma parameters on SVC, rbf kernel ')
-lab.study_svc(kernel='rbf', 
-              C_list=dynamic_C_list,
-              gamma_list=dynamic_gamma_list)
-lab.visualize_results(svc_rbf_dual_fname, interval=INTERVAL)
-print('   ... saved to file %s ' % svc_rbf_dual_fname)
+lab.svm_animation(
+    svm_=dict(impl='svc', kernel='rbf', dfs='ovr'),
+    range_=dict(C=dynamic_C_range, gamma=dynamic_gamma_range),
+    animation_=dict(filename=svc_rbf_both_fname)
+)
+print('   ... saved to file %s ' % svc_rbf_both_fname)
 
 # nusvc, linear kernel, study effect of nu parameter
 print('-- studying effect of nu parameter on NuSVC, linear kernel ')
-lab.study_nusvc(kernel='linear', nu_list=dynamic_nu_list)
-lab.visualize_results(nusvc_linear_nu_fname, interval=INTERVAL)
+lab.svm_animation(
+    svm_=dict(impl='nusvc', kernel='linear', dfs='ovr'),
+    range_=dict(nu=dynamic_nu_range, gamma=None),
+    animation_=dict(filename=nusvc_linear_nu_fname)
+)
 print('   ... saved to file %s ' % nusvc_linear_nu_fname)
+   
+#-------------------------------------------------------------------------------
+# svc, grid search with cross validation to find optimal parameters
+# linear
+C_range = np.logspace(-3, 9, 13)
+gamma_range = [0.0] # no gamma in linear
+print('-- svc, linear kernel, grid search for optimal parameters ')
+best_params, best_score = lab.optimal_param_grid_search(
+    svm_=dict(impl='svc', kernel='linear', dfs='ovr'),
+    range_=dict(C=C_range, gamma=gamma_range), 
+    heatmap_=dict(
+        filename=svc_linear_grid_search,
+        norm=colors.PowerNorm(gamma=5.)
+    )
+)
+print('   * the best parameters are %s with a score of %0.2f'
+          % (best_params, best_score))
+print('   ... saved to file %s ' % svc_linear_grid_search)
+
+''' **disabled because poly takes way too long **
+# poly 
+C_range = np.logspace(-2, 2, 5)
+gamma_range = np.logspace(-2, 2, 5)
+print('-- svc, poly kernel, grid search for optimal parameters ')
+best_params, best_score = lab.optimal_param_grid_search(
+    svm_=dict(impl='svc', kernel='poly', dfs='ovr'),
+    range_=dict(C=C_range, gamma=gamma_range), 
+    heatmap_=dict(filename=svc_poly_grid_search)
+)
+print('   * the best parameters are %s with a score of %0.2f'
+          % (best_params, best_score))
+print('   ... saved to file %s ' % svc_poly_grid_search)
+'''
+
+# rbf
+C_range = np.logspace(-3, 3, 7)
+gamma_range = np.logspace(-3, 3, 7)
+print('-- svc, rbf kernel, grid search for optimal parameters ')
+best_params, best_score = lab.optimal_param_grid_search(
+    svm_=dict(impl='svc', kernel='rbf', dfs='ovr'),
+    range_=dict(C=C_range, gamma=gamma_range), 
+    heatmap_=dict(filename=svc_rbf_grid_search)
+)
+print('   * the best parameters are %s with a score of %0.2f'
+          % (best_params, best_score))
+print('   ... saved to file %s ' % svc_rbf_grid_search)
+
+# nusvc, grid search with cross validation to find optimal parameters
+#lab.optimal_param_grid_search('nusvc', nu_range, gamma_range,
+#                              heatmap='nusvc_optimal_param_search.png')
 
 print('-- done ')
--- svmlab.py
+++ svmlab.py
@@ -1,19 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import colors
 from matplotlib.animation import FuncAnimation
-from sklearn.model_selection import train_test_split
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, GridSearchCV
 from sklearn.metrics import confusion_matrix
 from sklearn import svm
 
-''' 
-fit classifier and collect useful data and metadata
+KERNELS_WITH_GAMMA = ['rbf', 'poly', 'sigmoid']
 
+''' Fit classifier and collect useful data and metadata
 '''
 def run_classifier(clf, x_data, y_data):
-
-    # determine type of svm classifier
-    clf_type = clf._impl
-
     # split data for training and testing
     x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, random_state=0)
     
@@ -55,88 +53,231 @@ def run_classifier(clf, x_data, y_data):
     
     return results
 
-'''
-support vector machines student class
+def develop_svc_results(x_data, y_data, kernel, C, gamma, dfs='ovr'):
+    if kernel not in KERNELS_WITH_GAMMA:
+        clf = svm.SVC(kernel=kernel, C=C, decision_function_shape=dfs)
+    else:
+        clf = svm.SVC(kernel=kernel, C=C, gamma=gamma, decision_function_shape=dfs)
 
+    return run_classifier(clf, x_data, y_data)
+
+def develop_multiple_svc_results(x_data, y_data, kernel, C_range, gamma_range, dfs='ovr'):
+    # initialize results list
+    results_buffer = []
+
+    # if kernel does not use gamma, create arbitrary gamma_range to zip with C_range
+    if kernel not in KERNELS_WITH_GAMMA: 
+        gamma_range = [0.0 for _ in range(len(C_range))]
+
+    # C and gamma lists should have same length
+    assert len(C_range) == len(gamma_range)
+    
+    # iterate through C, gamma tuples and develop results    
+    for C, gamma in zip(C_range, gamma_range):
+        results_buffer.append(develop_svc_results(x_data, y_data, kernel, C, gamma, dfs=dfs))
+
+    return results_buffer
+
+def develop_nusvc_results(x_data, y_data, kernel, nu, gamma, dfs='ovr'):
+    if kernel not in KERNELS_WITH_GAMMA:
+        clf = svm.NuSVC(kernel=kernel, nu=nu, decision_function_shape=dfs)
+    else:
+        clf = svm.NuSVC(kernel=kernel, nu=nu, gamma=gamma, decision_function_shape=dfs)
+
+    return run_classifier(clf, x_data, y_data)
+
+def develop_multiple_nusvc_results(x_data, y_data, kernel, nu_range, gamma_range, dfs='ovr'):
+    # initialize resultslist
+    results_buffer = []
+
+    # if kernel does not use gamma, create arbitrary gamma_range to zip with nu_range
+    if kernel not in KERNELS_WITH_GAMMA: 
+        gamma_range = [0.0 for _ in range(len(nu_range))]
+
+    # nu and gamma lists should have same length
+    assert len(nu_range) == len(gamma_range)
+
+    # iterate through nu, gamma tuples and develop results
+    for nu, gamma in zip(nu_range, gamma_range):
+        if nu > 0.9:
+            print('svmlab: study cut short due to infeasible nu value (greater than 0.9)')
+            break
+        results_buffer.append(develop_nusvc_results(x_data, y_data, kernel, nu, gamma, dfs=dfs))
+
+    return results_buffer
+    
+''' Support vector machines student class
 '''
 class SVMLab:
     def __init__(self, x_data, y_data):
-        self.samples, self.labels = x_data, y_data
-        self.study = []
+        self.x_data, self.y_data = x_data, y_data
+        self.results_buffer = []
         self.fig = plt.figure(figsize=(8,6))
         self.ax = self.fig.add_subplot(111)
 
     def assign_new_data(x_data, y_data):
-        self.samples, self.labels = x_data, y_data
+        self.x_data, self.y_data = x_data, y_data
 
-    def study_svc(self, kernel='rbf', dfs='ovr',
-                  C_list=[1.], gamma_list=[1.]):
-        # empty study list to put new results
-        self.study = []
+    def svm_animation(self, 
+                      svm_=dict(impl='svc', kernel='rbf', dfs='ovr'),
+                      range_=dict(C=[1.], nu=[1.], gamma=[1.]),
+                      animation_=dict(filename='svm_animation.mp4', interval=0.5)):
 
-        # linear kernel does not use gamma, create arbitrary gamma_list to zip with C_list
-        if kernel in ['linear', 'sigmoid']: 
-            gamma_list = [0.0 for _ in range(len(C_list))]
+        if svm_['impl'] in ['svc', 'SVC', 'c_svc']:
+            self.results_buffer = develop_multiple_svc_results(
+                self.x_data, self.y_data, 
+                svm_['kernel'], range_['C'], range_['gamma'], dfs=svm_['dfs']
+            )
+        elif svm_['impl'] in ['nusvc', 'NuSVC', 'nu_svc']:
+            self.results_buffer = develop_multiple_nusvc_results(
+                self.x_data, self.y_data, 
+                svm_['kernel'], range_['nu'], range_['gamma'], dfs=svm_['dfs']
+            )
+        else:
+            print('svmlab: Unrecognized svm type')
+            assert False
 
-        # C and gamma lists should have same length
-        assert len(C_list) == len(gamma_list)
+        interval = 0.5
+        if 'interval' in animation_:
+            interval = animation_['interval'] #in seconds
+        anim = FuncAnimation(self.fig, self.draw_plot, self.report,
+                             interval=interval*1e+3, blit=False)
+        anim.save(animation_['filename'])
+        self.fig_reset()
 
-        # iterate through C, gamma tuples and develop results    
-        for C, gamma in zip(C_list, gamma_list):
-            if kernel in ['linear', 'sigmoid']:
-                clf = svm.SVC(kernel=kernel, C=C, decision_function_shape=dfs)
+        return
+
+    def svm_plot(self, 
+                 svm_=dict(impl='svc', kernel='rbf', C=1.0, gamma='auto', dfs='ovr'),
+                 plot_=dict(filename='svm_plot.mp4')):
+        
+        if svm_['impl'] in ['svc', 'SVC', 'c_svc']:
+            self.draw_plot(
+                develop_svc_results(
+                    self.x_data, self.y_data,
+                    svm_['kernel'], svm_['C'], svm_['gamma'], dfs=svm_['dfs']
+                )
+            )
+        elif svm_['impl'] in ['nusvc', 'NuSVC', 'nu_svc']:
+            self.draw_plot(
+                develop_nusvc_results(
+                    self.x_data, self.y_data,
+                    svm_['kernel'], svm_['nu'], svm_['gamma'], dfs=svm_['dfs']
+                )
+            )
+        else:
+            print('svmlab: Unrecognized svm type')
+            assert False
+
+        self.fig.savefig(plot_['filename'])
+        self.fig_reset()
+
+        return
+
+    def optimal_param_grid_search(self, #svm_obj, C_range, gamma_range, heatmap=None):
+                                  svm_=dict(impl='svc', kernel='rbf', dfs='ovr'),
+                                  range_=dict(C=[1.], nu=[1.], gamma=[1.]),
+                                  heatmap_=dict(
+                                    filename='svm_gridsearch.png',
+                                    norm=colors.PowerNorm(gamma=10.)
+                                  )):
+        # build the defined svm
+        svm_obj = None
+        param_grid = dict(gamma=range_['gamma'])
+        if svm_['impl'] in ['svc', 'SVC', 'c_svc']:
+            svm_obj = svm.SVC(kernel=svm_['kernel'], decision_function_shape=svm_['dfs'])
+            param_grid['C'] = range_['C']
+        elif svm_['impl'] in ['nusvc', 'NuSVC', 'nu_svc']:
+            svm_obj = svm.NuSVC(kernel=svm_['kernel'], decision_function_shape=svm_['dfs'])
+            param_grid['nu'] = range_['nu']
+        else:
+            print('svmlab: Unrecognized svm type')
+            assert False
+
+        # cross validation strategy
+        cross_validation = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+        
+        # perform grid search
+        grid = GridSearchCV(svm_obj, param_grid=param_grid, cv=cross_validation)
+        grid.fit(self.x_data, self.y_data)
+
+        # build grid search dict with results and metadata
+        grid_search_ = dict(
+            svm_=svm_,
+            range_=range_,
+            cv_results_=grid.cv_results_
+        )
+
+        # plot heatmap if defined
+        if heatmap_:
+            if 'norm' in heatmap_:
+                self.draw_heatmap(grid_search_, norm=heatmap_['norm'])
             else:
-                clf = svm.SVC(kernel=kernel, C=C, gamma=gamma, decision_function_shape=dfs)
+                self.draw_heatmap(grid_search_)
+            self.fig.savefig(heatmap_['filename'])
+            self.fig_reset()
 
-            results = run_classifier(clf, self.samples, self.labels)
-            self.study.append(results)
+        return grid.best_params_, grid.best_score_
 
-        return self.study
+    def draw_heatmap(self, grid_search, norm=colors.PowerNorm(gamma=10.)):
+        # clear previous heatmap
+        self.ax.clear()
 
-    def study_nusvc(self, kernel='rbf', dfs='ovr',
-                    nu_list=[1.], gamma_list=[1.]):
-        # empty study list to put new results
-        self.study = []
+        # adjust subplot
+        self.fig.subplots_adjust(left=.05, right=0.95, bottom=0.15, top=0.90)
 
-        # linear kernel does not use gamma, create arbitrary gamma_list to zip with nu_list
-        if kernel in ['linear', 'sigmoid']: 
-            gamma_list = [0.0 for _ in range(len(nu_list))]
+        # svm metadata
+        impl = grid_search['svm_']['impl']
+        kernel = grid_search['svm_']['kernel']
 
-        # nu and gamma lists should have same length
-        assert len(nu_list) == len(gamma_list)
+        # parameter ranges
+        C_range = grid_search['range_']['C']
+        gamma_range = grid_search['range_']['gamma']
 
-        # iterate through nu, gamma tuples and develop results    
-        for nu, gamma in zip(nu_list, gamma_list):
-            if nu > 0.9:
-                print('svmlab: study cut short due to infeasible nu value (greater than 0.9)')
-                break
-            if kernel in ['linear', 'sigmoid']:
-                clf = svm.NuSVC(kernel=kernel, nu=nu, decision_function_shape=dfs)
-            else:
-                clf = svm.NuSVC(kernel=kernel, nu=nu, gamma=gamma, decision_function_shape=dfs)
+        # cross-validation results
+        cv_results_ = grid_search['cv_results_']
 
-            results = run_classifier(clf, self.samples, self.labels)
-            self.study.append(results)
+        scores = cv_results_['mean_test_score'].reshape(len(C_range), len(gamma_range))
+        vmin = scores.min()
+        midpoint = (scores.max() + scores.min()) * 0.5
 
-        return self.study
+        # heatmap
+        heatmap = self.ax.imshow(scores, interpolation='nearest', cmap=plt.cm.hot, norm=norm)
+        self.ax.set_xlabel('gamma')
+        self.ax.set_ylabel('C')
+        self.ax.set_xticks(np.arange(len(gamma_range)))
+        self.ax.set_xticklabels(gamma_range)
+        self.ax.set_yticks(np.arange(len(C_range)))
+        self.ax.set_yticklabels(C_range)
+        #self.ax.tick_params(axis='x', labelrotation=45)
+        for tick in self.ax.get_xticklabels():
+            tick.set_rotation(45)
+        self.ax.set_title('Validation Accuracy')
 
-    def animate(self, results):
-        # clear plot
+        # colorbar
+        divider = make_axes_locatable(self.ax)
+        cax = divider.append_axes('right', size='5%', pad=0.05)
+        self.fig.colorbar(heatmap, cax=cax, orientation='vertical')
+
+        return
+
+    def draw_plot(self, result):
+        # clear previous plot
         self.ax.clear()
 
         # classifier metadata 
-        metadata = results['metadata']
-        clf_type = metadata['_impl']
+        metadata = result['metadata']
+        impl = metadata['_impl']
         kernel = metadata['kernel']
         C, nu, gamma = metadata['C'], metadata['nu'], metadata['gamma']
 
         # get plot data
-        x_train, y_train = results['training set']
-        x_test, y_test = results['testing set']
-        x_support = results['support']
-        XX, YY, Z = results['contour']
-        score = results['score']
-        x_min, x_max, y_min, y_max = results['bounds']
+        x_train, y_train = result['training set']
+        x_test, y_test = result['testing set']
+        x_support = result['support']
+        XX, YY, Z = result['contour']
+        score = result['score']
+        x_min, x_max, y_min, y_max = result['bounds']
 
         # update plot
         # training set
@@ -152,21 +293,22 @@ class SVMLab:
 
         # title
         title = kernel + ' kernel:' 
-        if clf_type == 'c_svc':
+        if impl == 'c_svc':
             title += (' C=%8.3f' % C)
-        elif clf_type == 'nu_svc':
+        elif impl == 'nu_svc':
             title += (' nu=%8.3f' % nu)
         else:
-            print('svmlab: Unrecognized classifier type')
+            print('svmlab: Unrecognized svm type')
+            assert(False)
         
-        if kernel not in ['linear', 'sigmoid']: # linear and sigmoid kernel do not include gamma
+        if kernel in KERNELS_WITH_GAMMA: 
             title += (', gamma=%8.3f ' % gamma)
         self.ax.set_title(title)
 
         # legend
-        self.ax.legend([train_scat, test_scat, supp_scat],
-            ['training set', 'testing set', 'supports'],
-            bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+#        self.ax.legend([train_scat, test_scat, supp_scat],
+#            ['training set', 'testing set', 'supports'],
+#            bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 
         # annotations
         font = {
@@ -180,18 +322,16 @@ class SVMLab:
         self.ax.text(x_max-1.065, y_max-0.68, r'supports: %03d' % len(x_support), fontsize=12,
             bbox={'facecolor':'white', 'alpha':0.8, 'pad':13.3})
 
-
         return
+
+    def fig_reset(self):
+        self.fig.clear()
+        self.fig = plt.figure(figsize=(8,6))
+        self.ax = self.fig.add_subplot(111)
       
-    def report(self):
-        for results in self.study:
-            yield results
-
-    def visualize_results(self, filename, interval=0.5):
-        interval = interval #in seconds
-        anim = FuncAnimation(self.fig, self.animate, self.report,
-                             interval=interval*1e+3, blit=False)
-        anim.save(filename)
-
         return
+
+    def report(self):
+        for results in self.results_buffer:
+            yield results
 			
